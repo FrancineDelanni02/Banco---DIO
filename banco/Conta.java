cb1541f82f7415ca483146c23c24b3f3a11d90ae
FrancineDelanni02 <delannifrancine@gmail.com>
2025-04-12 03:21:39 -0300

    create function tranfesrir, depositar, sacar

--- banco/Conta.java
+++ banco/Conta.java
@@ -1,22 +1,38 @@
 package banco;
 
 public abstract class Conta implements InterfaceConta{
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
 
     private int numero;
     private int agencia;
     private double saldo;
 
-    public boolean sacar(){
+    public Conta(){
+       this.numero = SEQUENCIAL++;
+       this.agencia = Conta.AGENCIA_PADRAO;
+       this.saldo = 0;
+    }
+
+    public boolean sacar(double valor){
+        if(saldo < valor)
+            return false;
+
+        saldo -= valor;
         return true;
     }
 
-    public boolean depositar(){
+    public boolean depositar(double valor){
+        saldo += valor;
         return true;
     }
 
-    public boolean transferir(){
-        return true;
-    }
+    public boolean transferir(double valor,Conta contaDestino){
+        if(sacar(valor))
+            contaDestino.depositar(valor);
+
+        return false;
+        }
 
     public int getNumero() {
         return numero;
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
 package src;
+import banco.*;
 
 public class Main {
+    public static void main(String[] args){
+        Conta cc = new ContaCorrente();
+        Conta cp = new ContaPoupanca();
+    }
 }
